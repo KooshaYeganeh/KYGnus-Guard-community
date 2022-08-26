ea88940eaef9c8a3de76d26564c09fccdfaa7cac
kooshakooshadv <kooshakooshadv@gmail.com>
2022-08-26 18:44:26 +0430

    config File

---
+++ config.py
@@ -0,0 +1,10 @@
+USERNAME = "koosha"
+PASSWORD = "102030"
+ADMIN_USERNAME="admin"
+ADMIN_PASSWORD="admin"
+DB_HOST = "127.0.0.1"
+DB_USER = "root"
+DB_PASSWORD = "Koosha_102030"
+DB = "malware"
+DB_PORT=3360
+SECRET_KEY = "Yoosha Yeganeh Gnu Linux Softwares"
