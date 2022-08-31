1bb304c837a6fe5748ab604409c81a2600b01e2d
kooshakooshadv <kooshakooshadv@gmail.com>
2022-08-31 12:19:14 +0430

    Runner File

---
+++ KYGnus_Guard.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+
+
+<<username
+Enter Your Youser and App Files will be in This User
+username
+
+username="" # Enter username
+
+
+cd /home/$username/App/KYGnus_Guard_community
+
+source venv/bin/activate
+
+<<python
+Note : if You User Debian Base systems Like Ubunjtu or Lubuntu or Others
+Yoy should Change python with python3 tu Run App.in RedHat Base systems Like
+Fedora python command Runs Default Python3
+python
+
+python app.py
+
