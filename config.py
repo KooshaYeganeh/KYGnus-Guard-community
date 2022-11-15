ceaf7ec0f52d15462269deddd97309d19a12ea10
kooshakooshadv <kooshakooshadv@gmail.com>
2022-11-15 23:14:53 +0330

    Add Comments to config File

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
+# Edit this File With Your Configurations
+# USERNAME = username for Login in App
 USERNAME = "koosha"
+# PASSWORD = Password for Login in App
 PASSWORD = "102030"
 ADMIN_USERNAME="admin"
 ADMIN_PASSWORD="admin"
+# if Your Database Seperated From Application Server You Can Enter ip Address of Database in Remote Server
 DB_HOST = "127.0.0.1"
-DB_USER = "root"
-DB_PASSWORD = "Koosha_102030"
+# MYSQL Database user
+DB_USER = "root" 
+# MYSQL database Password
+DB_PASSWORD = "123123" # add Your Database password Here
 DB = "malware"
+# MYSQL Port
+## At Default The Port of Mysql is 3306 But For Security Reasons Thats Better To chaneg port of MYSQL
 DB_PORT=3360
 SECRET_KEY = "Koosha Yeganeh Gnu Linux Softwares"
 
 
-
-ssh_port= "22"
-ssh_host= "192.168.1.101"
-ssh_username= "hamed"
-ssh_password = "H102030h"
